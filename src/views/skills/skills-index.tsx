810b9b84092ef73d37125160eb75cbd5e5f3c31b
itspoli <poli@itspoli.com>
2019-06-21 02:11:19 -0700

    spacing text fixed up

--- src/views/skills/skills-index.tsx
+++ src/views/skills/skills-index.tsx
@@ -37,21 +37,21 @@ export class SkillsIndex extends React.Component<Props, State> {
   render() {
     return (
       <div className={`skills content ${this.state.loaded ? "loaded" : "loading"}`}>
-        <Link to="/"><svg className="iconClose" enable-background="new 0 0 100 100" version="1.1" viewBox="0 0 100 100" height="28px" width="28px"><polygon points="77.6,21.1 49.6,49.2 21.5,21.1 19.6,23 47.6,51.1 19.6,79.2 21.5,81.1 49.6,53 77.6,81.1 79.6,79.2   51.5,51.1 79.6,23 "/></svg></Link>
+        <Link to="/"><svg className="iconClose" enable-background="new 0 0 100 100" version="1.1" viewBox="0 0 100 100" height="28px" width="28px"><polygon points="77.6,21.1 49.6,49.2 21.5,21.1 19.6,23 47.6,51.1 19.6,79.2 21.5,81.1 49.6,53 77.6,81.1 79.6,79.2   51.5,51.1 79.6,23 " /></svg></Link>
         <h1>Skills</h1>
         <ul>
-          <li className="icon"><img src={iconHtml} alt="Html5 icon"/></li>
-          <li className="icon"><img src={iconCss} alt="Css3 icon"/></li>
-          <li className="icon"><img src={iconSass} alt="Sass icon"/></li>
-          <li className="icon"><img src={iconJavascript} alt="Javascript icon"/></li>
-          <li className="icon"><img src={iconTypescript} alt="Typescript icon"/></li>
-          <li className="icon"><img src={iconReact} alt="React js icon"/></li>
-          <li className="icon"><img src={iconNode} alt="Node js icon"/></li>
-          <li className="icon"><img src={iconGit} alt="Git icon"/></li>
-          <li className="icon"><img src={iconGithub} alt="Git hub icon"/></li>
-          <li className="icon"><img src={iconNpm} alt="Npm icon"/></li>
-          <li className="icon"><img src={iconYarn} alt="Yarn icon"/></li>
-        </ul>       
+          <li className="icon"><img src={iconHtml} alt="Html5 icon" /></li>
+          <li className="icon"><img src={iconCss} alt="Css3 icon" /></li>
+          <li className="icon"><img src={iconSass} alt="Sass icon" /></li>
+          <li className="icon"><img src={iconJavascript} alt="Javascript icon" /></li>
+          <li className="icon"><img src={iconTypescript} alt="Typescript icon" /></li>
+          <li className="icon"><img src={iconReact} alt="React js icon" /></li>
+          <li className="icon"><img src={iconNode} alt="Node js icon" /></li>
+          <li className="icon"><img src={iconGit} alt="Git icon" /></li>
+          <li className="icon"><img src={iconGithub} alt="Git hub icon" /></li>
+          <li className="icon"><img src={iconNpm} alt="Npm icon" /></li>
+          <li className="icon"><img src={iconYarn} alt="Yarn icon" /></li>
+        </ul>
       </div>
     );
   }
